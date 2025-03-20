13100cbb7827d11d1c70844a7122088554d46322
Bolu Adalemo <boluadalemo@gmail.com>
2025-03-20 14:12:43 -0300

    vault backup: 2025-03-20 14:12:43

--- School/Computer Programming 12/ProfitTracker/ProfitTracker.java
+++ School/Computer Programming 12/ProfitTracker/ProfitTracker.java
@@ -8,6 +8,28 @@ import java.util.List;
  * Ask teacher if there is need to write comments to explain methods,
    Since i am already writing a brief report
 */
+
+/*INTRODUCED IN PART 1
+ * The class ProfitTracker is used to track the profits of a business for a week.
+ * Before calling methods, i made a list called dailyProfits, and a scanner called in.
+ * In Part 1, the class was created with the following methods:
+ * 1. enterDailyProfits - This method prompts the user to enter daily profits until the user enters -1.
+ * 2. displayTotalProfit - This method calculates and displays the total weekly profit.
+*/
+
+/*INTRODUCED IN PART 2
+ * Before adding the new methods, I added an instance variable for weekly goal.
+ * In part 2, the ProfitTracker class was updated with the following methods:
+ * 1. setWeeklyGoal - This method prompts the user to enter the weekly profit goal.
+ * 2. checkGoalStatus - This method checks if the weekly profit goal was met and displays a message accordingly.
+*/
+
+/*INTRODUCED IN PART 3
+ * I added an instance variable for weekly profit.
+ * In part 3, the ProfitTracker class was updated with the following methods:
+ * 1. displayRemainingAmount - This method calculates and displays the remaining profit needed to reach the weekly goal.
+ * 2. I updated the enterDailyProfits method to call the displayRemainingAmount method after each daily profit is entered.
+*/
 public class ProfitTracker
 {
     public static void main(String[] args)
