4862bea0a94e61b4a337e373827494b2ea4fbbb6
Bolu Adalemo <boluadalemo@gmail.com>
2025-03-20 14:59:43 -0300

    vault backup: 2025-03-20 14:59:43

--- School/Computer Programming 12/ProfitTracker/ProfitTracker.java
+++ School/Computer Programming 12/ProfitTracker/ProfitTracker.java
@@ -30,98 +30,82 @@ import java.util.List;
  * 1. displayRemainingAmount - This method calculates and displays the remaining profit needed to reach the weekly goal.
  * 2. I updated the enterDailyProfits method to call the displayRemainingAmount method after each daily profit is entered.
 */
-public class ProfitTracker
-{
-    public static void main(String[] args)
-    {
-        ProfitTracker tracker = new ProfitTracker(); //Create an instance of Profit Tracker
-        tracker.setWeeklyGoal(); //Call setWeeklyGoal
-        tracker.enterDailyProfits(); //Call enterDailyProfits
-        tracker.displayTotalProfit(); //Call displayTotalProfit
-        tracker.checkGoalStatus(); //Call checkGoalStatus
+
+public class ProfitTracker {
+    public static void main(String[] args) {
+        ProfitTracker tracker = new ProfitTracker(); // Create an instance of Profit Tracker
+        tracker.setWeeklyGoal(); // Call setWeeklyGoal
+        tracker.enterDailyProfits(); // Call enterDailyProfits
+        tracker.displayTotalProfit(); // Call displayTotalProfit
+        tracker.checkGoalStatus(); // Call checkGoalStatus
     }
+
     private List<Double> dailyProfits;
     private Scanner in;
     private double weeklyGoal; // Instance variable for weekly goal
-    private double weeklyProfit; //Instance variable for weekly profit
+    private double weeklyProfit; // Instance variable for weekly profit
 
-    public ProfitTracker()
-    {
+    public ProfitTracker() {
         this.dailyProfits = new ArrayList<>();
         double weeklyGoal;
         this.in = new Scanner(System.in); // Initialize the scanner
     }
 
-    public void enterDailyProfits()
-    {
+    public void enterDailyProfits() {
         System.out.println("Enter daily profits (enter -1 to stop):");
 
         double profit;
-        while (true)
-        {
+        while (true) {
             System.out.print("Profit: ");
             profit = in.nextDouble();
 
-            if (profit == -1)
-            {
-                break; //Exit the loop if the user enters -1
+            if (profit == -1) {
+                break; // Exit the loop if the user enters -1
             }
 
             dailyProfits.add(profit);
             displayRemainingAmount();
         }
     }
-    public void setWeeklyGoal()
-    {
+
+    public void setWeeklyGoal() {
         System.out.print("What is the Profit Goal for the week? ");
         this.weeklyGoal = in.nextDouble();
 
     }
-    public void displayTotalProfit()
-    {
+
+    public void displayTotalProfit() {
         double totalProfit = 0;
-        for (double profit: dailyProfits)
-        {
+        for (double profit : dailyProfits) {
             totalProfit += profit;
         }
         this.weeklyProfit = totalProfit;
         System.out.println("Total weekly profit $" + totalProfit);
     }
-    public void checkGoalStatus()
-    {
-        if (weeklyProfit > weeklyGoal)
-        {
+
+    public void checkGoalStatus() {
+        if (weeklyProfit > weeklyGoal) {
             System.out.println("You passed the weekly goal of $" + weeklyGoal + "! Woohoo!");
-        }
-        else if (weeklyProfit < weeklyGoal)
-        {
+        } else if (weeklyProfit < weeklyGoal) {
             double difference = weeklyGoal - weeklyProfit;
             System.out.println("You needed $" + difference + " more to hit your weekly goal!");
-        }
-        else
-        {
-            System.out.println("You hit your weekly goal of $" + weeklyGoal +"! Woohoo!");
+        } else {
+            System.out.println("You hit your weekly goal of $" + weeklyGoal + "! Woohoo!");
         }
     }
-    private void displayRemainingAmount()
-    {
+
+    private void displayRemainingAmount() {
         double currentTotal = 0;
-        for (double profit : dailyProfits)
-        {
+        for (double profit : dailyProfits) {
             currentTotal += profit;
         }
 
         double remaining = weeklyGoal - currentTotal;
-        if (currentTotal > weeklyGoal)
-        {
+        if (currentTotal > weeklyGoal) {
             System.out.println("You have exceeded weekly goal!");
-        }
-        else if (currentTotal < weeklyGoal)
-        {
+        } else if (currentTotal < weeklyGoal) {
             System.out.println("Remaining profit needed: $" + remaining);
-        }
-        else
-        {
+        } else {
             System.out.println("You reached your weekly goal!");
         }
     }
